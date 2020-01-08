9db3ea286020e8087ca293e4381bc363af0868b2
Roman Firsov <rf@anadeainc.com>
2020-01-08 22:29:17 +0200

    Remove halson object generator.

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,26 +1,9 @@
-const halson = require('halson');
 const BaseController = require('./baseController');
 
 class IndexController extends BaseController {
   async index() {
-    const indexURI = this.uriGenerator.getURI(
-      'IndexController_index',
-      {},
-      '_self',
-    );
-
-    const resource = halson({ api: 'api v1' });
+    const resource = {};
     try {
-      const links = await Promise.all([
-        indexURI,
-      ]);
-  
-      links.forEach((link) => {
-        if (link) {
-          resource.addLink(link.id, link);
-        }
-      });
-
       super.ok(resource);
     } catch (err) {
       super.error(err);
