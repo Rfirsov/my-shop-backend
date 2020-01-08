b136b5d810b87d2436c18a3ba8d8f74fe835c029
Roman Firsov <rf@anadeainc.com>
2020-01-08 15:19:20 +0200

    Remove unuse logic.

--- controllers/index.js
+++
@@ -1,9 +0,0 @@
-// const express = require('express');
-// const IndexRouter = require('../routes/Router');
-// const app = express();
-
-// IndexRouter.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-
-// module.exports = IndexRouter;
--- controllers/indexController.js
+++ controllers/indexController.js
@@ -4,37 +4,17 @@ const BaseController = require('./baseController');
 class IndexController extends BaseController {
   async index() {
     const indexURI = this.uriGenerator.getURI(
-        'IndexController_index',
-        {},
-        '_self',
+      'IndexController_index',
+      {},
+      '_self',
     );
-
-    // const getBooksURI = this.uriGenerator.getURI(
-    //     'BooksListController_getBooks',
-    // );
-
-    // const getBookURI = this.uriGenerator.getURI(
-    //     'BooksListController_getBook',
-    // );
-
-    // const rateBookURI = this.uriGenerator.getURI(
-    //     'BooksListController_rateBook',
-    // );
-
-    // const removeBookURI = this.uriGenerator.getURI(
-    //     'BooksListController_removeBook',
-    // );
 
     const resource = halson({ api: 'api v1' });
     try {
       const links = await Promise.all([
         indexURI,
-        // getBooksURI,
-        // getBookURI,
-        // rateBookURI,
-        // removeBookURI,
       ]);
-
+  
       links.forEach((link) => {
         if (link) {
           resource.addLink(link.id, link);
